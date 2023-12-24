f65b584a684b9c710cfc679b17a46dd37f47c130
Matt Walker <matt.walker@genesis.global>
2023-12-24 16:41:47 +0000

    feat: component markup and lifecycle

--- 01_defining_web_components/my_counter/my-counter.js
+++ 01_defining_web_components/my_counter/my-counter.js
@@ -1,8 +1,43 @@
 class MyCounter extends HTMLElement {
+	static #fragment = null;
+	#view = null;
+
+	static observedAttributes = ["count"];
+	#countDisplay = null;
+
   constructor() {
     super();
-    this.attachShadow({ mode: "open" }).innerHTML = "Hello World!";
+    this.attachShadow({ mode: "open" });
   }
+
+	connectedCallback() {
+		if (this.#view === null) {
+			this.#view = this.#createView();
+			this.shadowRoot.appendChild(this.#view);
+		}
+		this.#countChanged();
+	}
+
+	attributeChangedCallback(name, oldValue, newValue) {
+		if (name === "count") {
+			this.#countChanged();
+		}
+	}
+
+	#countChanged() {
+		if (this.#countDisplay == null) {
+			this.#countDisplay = this.shadowRoot.getElementById("count");
+		}
+		this.#countDisplay.textContent = this.getAttribute("count");
+	}
+
+	#createView() {
+		if (MyCounter.#fragment === null) {
+			const template = document.getElementById("my-counter");
+			MyCounter.#fragment = document.adoptNode(template.content);
+		}
+		return MyCounter.#fragment.cloneNode(true);
+	}
 }
 
 customElements.define("my-counter", MyCounter);
