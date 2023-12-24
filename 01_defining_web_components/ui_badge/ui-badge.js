e27d9b81ac194d017081af2a47f7e6777678bd4c
Matt Walker <matt.walker@genesis.global>
2023-12-24 17:55:52 +0000

    feat: finish component

--- 01_defining_web_components/ui_badge/ui-badge.js
+++ 01_defining_web_components/ui_badge/ui-badge.js
@@ -1,8 +1,26 @@
 class UiBadge extends HTMLElement {
+	static #fragment = null;
+	#view = null;
+
   constructor() {
     super();
-    this.attachShadow({ mode: "open" }).innerHTML = "Hello World!";
+    this.attachShadow({ mode: "open" });
   }
+
+	connectedCallback() {
+		if (this.#view == null) {
+			this.#view = this.#createView();
+			this.shadowRoot.appendChild(this.#view);
+		}
+	}
+
+	#createView() {
+		if (UiBadge.#fragment == null) {
+			const template = document.getElementById("ui-badge");
+			UiBadge.#fragment = document.adoptNode(template.content);
+		}
+		return UiBadge.#fragment.cloneNode(true);
+	}
 }
 
 customElements.define("ui-badge", UiBadge);
